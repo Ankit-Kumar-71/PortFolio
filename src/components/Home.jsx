fa1236872bee3be4f5de5e0512e57ea7e57b30d6
Ankit-Kumar-71 <jeetkashyap20224@gmail.com>
2025-07-03 13:34:39 +0530

    write better code in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,21 +23,19 @@ const Home = () => {
           <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
             <div className="flex space-x-1 text-2xl md:text-4xl">
               <h1>Hello, I am a </h1>
-              {/* <span >Developer</span> */}
               <ReactTyped
                 className="text-red-700 font-bold px-1"
                 strings={["Developer", "Programmer", "Coder"]}
                 typeSpeed={40}
                 backSpeed={50}
-                loop={true}
+                loop
               />
-            </div>
-            <br />
-            <p className="text-sm md:text-md text-justify">
-              Highly motivated and detail-oriented Full Stack Developer with a strong foundation in Java,JavaScript and modern full stack Technologies.BCA graduate from Maharishi Dayanand University passionate about buliding scalable,efficient and user-friendly web application.Adept at working in collaborative,agile terms and committed to continuous learning and implemeting best coding practices across both frontend and beckend development.Seeking to leverage my skills and knowledge to contribute to innovative projects and drive positive impact in the software development industry. 
-            </p>
-            <br />
-            {/* social media icons */}
+              </div>
+              <br />
+              <p className="text-sm md:text-md text-justify">
+                Highly motivated and detail-oriented Full Stack Developer with a strong foundation in Java, JavaScript, and modern full stack technologies. BCA graduate from Maharishi Dayanand University, passionate about building scalable, efficient, and user-friendly web applications. Adept at working in collaborative, agile teams and committed to continuous learning and implementing best coding practices across both frontend and backend development. Seeking to leverage my skills and knowledge to contribute to innovative projects and drive positive impact in the software development industry.
+              </p>
+              <br />
             <div className="flex flex-col items-center md:flex-row justify-center space-y-6 md:space-y-0">
               <div className="space-y-2">
                 <h1 className="font-bold text-center ">Available on</h1>
